d0aee0de1b021c7c1d4ea9a671ed2d78d30e9279
KevinPMyers <kmyers25@gmail.com>
2021-07-01 21:10:07 -0400

    Map is added, still need to add logic to accept use of location with modal

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,54 @@
-var API_KEY = '22307283-f80c47e3ffcfb385f49e3498e';
-var URL = "https://pixabay.com/api/?key="+API_KEY+"&q="+encodeURIComponent('red roses');
-$.getJSON(URL, function(data){
-if (parseInt(data.totalHits) > 0)
-    $.each(data.hits, function(i, hit){ console.log(hit.pageURL); });
-else
-    console.log('No hits');
-});
+
+
+
+
+
+
+
+
+
+
+
+//   function calcRoute() {
+//     var start = document.getElementById('start').value;
+//     var end = document.getElementById('end').value;
+//     var request = {
+//       origin: start,
+//       destination: end,
+//       travelMode: 'BICYCLING'
+//     };
+//     directionsService.route(request, function(result, status) {
+//       if (status == 'OK') {
+//         directionsRenderer.setDirections(result);
+//       }
+//     });
+//   }
+
+function initMap() {
+    //var directionsService = new google.maps.DirectionsService();
+    //var directionsRenderer = new google.maps.DirectionsRenderer();
+    var peninsula = new google.maps.LatLng(41.237, -81.553);
+    var mapOptions = {
+      center: peninsula,
+      zoom: 14,
+      mapId: 'f195a7ab0618472c'
+    };
+    
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    //directionsRenderer.setMap(map);
+    const bikeLayer = new google.maps.BicyclingLayer();
+    bikeLayer.setMap(map);
+}
+
+// function createPhotoMarker(place) {
+//     var photos = place.photos;
+//     if (!photos) {
+//       return;
+//     }
+//     var marker = new google.maps.Marker({
+//       map: map,
+//       position: place.geometry.location,
+//       title: place.name,
+//       icon: photos[0].getUrl({maxWidth: 35, maxHeight: 35})
+//     });
+//   }
