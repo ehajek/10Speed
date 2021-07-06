ffb9c71b03f6694642d3fc7f9d38db165faff066
ImaniJo-code <imani.l.johnson@gmail.com>
2021-07-06 15:43:32 -0400

    geolocation

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,39 @@ function initMap() {
     zoom: 14,
     mapId: 'f195a7ab0618472c'
   });
-  const bikeLayer = new google.maps.BicyclingLayer();
-  bikeLayer.setMap(map);
+  infoWindow = new google.maps.InfoWindow();
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "Get My Location";
+  document.getElementById("location-button").appendChild(locationButton);
+  locationButton.addEventListener("click", () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          infoWindow.setPosition(pos);
+          infoWindow.setContent("Location found.");
+          infoWindow.open(map);
+          map.setCenter(pos);
+        },
+        () => {
+          handleLocationError(true, infoWindow, map.getCenter());
+        }
+      );
+    } else {
+      handleLocationError(false, infoWindow, map.getCenter());
+    }
+  });
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos){
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+    ? "Error: The Geolocation service failed."
+    : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
