bbd11623809702bc591547a16965ac562003b788
EddieH <ehajek@outlook.com>
2021-07-06 17:31:16 -0400

    Map Marker

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+
+var map;
+var marker = false;
+
 function initMap() {
   var peninsula = new google.maps.LatLng(41.237, -81.553);
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -7,4 +11,29 @@ function initMap() {
   });
   const bikeLayer = new google.maps.BicyclingLayer();
   bikeLayer.setMap(map);
+  google.maps.event.addListener(map, 'click', function(event) {                
+      var clickedLocation = event.latLng;
+      if(marker === false){
+          marker = new google.maps.Marker({
+              position: clickedLocation,
+              map: map,
+              draggable: true
+          });
+          google.maps.event.addListener(marker, 'dragend', function(event){
+              markerLocation();
+          });
+      } else{
+          marker.setPosition(clickedLocation);
+      }
+      markerLocation();
+  });
 }
+     
+function markerLocation(){
+  var currentLocation = marker.getPosition();
+  document.getElementById('lat').value = currentLocation.lat();
+  document.getElementById('lng').value = currentLocation.lng();
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
+ 
