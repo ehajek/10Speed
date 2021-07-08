db969f838bcdd3e6878b9cd7a5b2dbe40190b8fa
EddieH <ehajek@outlook.com>
2021-07-07 23:23:32 -0400

    Demo Code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,59 @@
-
-var map;
+var latCords = 41.237
+var lngCords = -81.553
 var marker = false;
+var originLocation = { lat: latCords, lng: lngCords };
 
 function initMap() {
-  var peninsula = new google.maps.LatLng(41.237, -81.553);
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: peninsula,
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: originLocation,
     zoom: 14,
     mapId: 'f195a7ab0618472c'
   });
-  const bikeLayer = new google.maps.BicyclingLayer();
-  bikeLayer.setMap(map);
-  google.maps.event.addListener(map, 'click', function(event) {                
-      var clickedLocation = event.latLng;
-      if(marker === false){
-          marker = new google.maps.Marker({
-              position: clickedLocation,
-              map: map,
-              draggable: true
-          });
-          google.maps.event.addListener(marker, 'dragend', function(event){
-              markerLocation();
-          });
-      } else{
-          marker.setPosition(clickedLocation);
-      }
-      markerLocation();
+  google.maps.event.addListener(map, 'click', function(event) {
+    clickedLocation = event.latLng;
+    markerLocation(map);
   });
 }
-     
-function markerLocation(){
-  var currentLocation = marker.getPosition();
-  document.getElementById('lat').value = currentLocation.lat();
-  document.getElementById('lng').value = currentLocation.lng();
-}
 
-google.maps.event.addDomListener(window, 'load', initMap);
- 
+function markerLocation(mapMaker){
+  var directionsService = new google.maps.DirectionsService();
+  var directionsRenderer = new google.maps.DirectionsRenderer({ map: mapMaker });
+  //directionsRenderer.setMap(mapMaker);
+  directionsService.route({
+    origin: originLocation,
+    destination: clickedLocation,
+    travelMode: google.maps.TravelMode.BICYCLING,
+  },(response, status) => {
+    console.log(response);
+    console.log(status);}
+  
+  ).then(function(Response){
+    directionsRenderer.setDirections(Response);
+    var turnsContainer = document.getElementById("turns");
+    var turnsEL = document.createElement('p');
+    var tripDistance = Response.routes[0].legs[0].distance.text;
+    var tripDuration = Response.routes[0].legs[0].duration.text;
+    console.log(tripDistance);
+    turnsContainer.innerHTML = "Distance: " + tripDistance + "<br/>" + "Duration: " +  tripDuration;
+    turnsContainer.appendChild(turnsEL);
+    steps = Response.routes[0].legs[0].steps;
+    //this.steps.forEach(i => console.log(steps[i].instructions));
+
+    function turnByTurnSteps (steps) {
+      var directions = document.getElementById('turnByTurn');
+      directions.innerHTML = '';
+      for (var i = 0; i < steps.length; i++) {
+        console.log(steps[i].instructions);
+        directions.innerHTML += '<br/><br/>' + steps[i].instructions + '<br/>' + steps[i].distance.text;
+        }
+      }
+     
+      turnByTurnSteps (Response.routes[0].legs[0].steps);
+
+  })};
+
+  //document.getElementById('startlat').value = originLocation.lat;
+  //document.getElementById('startlng').value = originLocation.lng;
+  //document.getElementById('lat').value = clickedLocation.lat;
+  //document.getElementById('lng').value = clickedLocation.lng;
+
